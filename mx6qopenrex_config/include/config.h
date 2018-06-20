96ca080c7c3e58701e87573e4aebc0b5f2b7658b
moisesPizarro <moises.antonio.pizarro@gmail.com>
2018-06-20 11:24:11 -0500

    Added support for SOLO

---
+++ mx6qopenrex_config/include/config.h
@@ -0,0 +1,9 @@
+/* Automatically generated - do not edit */
+#define CONFIG_IMX_CONFIG	board/fedevel/mx6openrex/mx6q_4x_mt41j256.cfg
+#define CONFIG_MX6Q	1
+#define CONFIG_BOARDDIR board/fedevel/mx6openrex
+#include <config_defaults.h>
+#include <config_uncmd_spl.h>
+#include <configs/mx6openrex.h>
+#include <asm/config.h>
+#include <config_fallbacks.h>
---
+++ mx6qopenrex_config/include/generated/timestamp_autogenerated.h
@@ -0,0 +1,3 @@
+#define U_BOOT_DATE "Jun 19 2018"
+#define U_BOOT_TIME "21:19:43"
+#define U_BOOT_TZ "-0500"
